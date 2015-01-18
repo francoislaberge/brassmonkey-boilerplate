fa34e47e1cd3aac18e0c30a435ba7790f29ca6bc
Francois Laberge <flaberge@brightcove.com>
2015-01-17 22:26:30 -0500

    ...

--- bin/to.sh
+++ bin/to.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
 cp chromecast-receiver.html ../browserdrop/www/receiver/receiver.html
+cp styles/main.css ../browserdrop/www/receiver/styles/main.css
+cp scripts/chromecast-receiver.js ../browserdrop/www/receiver/scripts/chromecast-receiver.js
+cp scripts/app.js ../browserdrop/www/receiver/scripts/app.js
+cp scripts/mario.js ../browserdrop/www/receiver/scripts/mario.js
