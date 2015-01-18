29b0d1efaf1052385e2851b7b71fea3d193b584b
Francois Laberge <flaberge@brightcove.com>
2015-01-17 23:43:24 -0500

    Removed Chromecast features. It wasn't working very well and makes this project more complex

--- bin/from.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-cp ../browserdrop/www/receiver/receiver.html chromecast-receiver.html
--- bin/to.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-cp chromecast-receiver.html ../browserdrop/www/receiver/receiver.html
-cp styles/main.css ../browserdrop/www/receiver/styles/main.css
-cp scripts/chromecast-receiver.js ../browserdrop/www/receiver/scripts/chromecast-receiver.js
-cp scripts/app.js ../browserdrop/www/receiver/scripts/app.js
-cp scripts/mario.js ../browserdrop/www/receiver/scripts/mario.js
-
-# Copy the controller over
-cp -r controller ../browserdrop/www/receiver/
